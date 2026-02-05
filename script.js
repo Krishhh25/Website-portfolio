0ca815a5b0df552c6a9d0815c693adb5cb616f25
Krish Shrivastav <shrivastavkrish15@gmail.com>
2026-02-06 02:17:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleDropdown() {
+    document.getElementById("contactDropdown").classList.toggle("show");
+}
+
+// Close dropdown if user clicks anywhere else on the screen
+window.onclick = function(event) {
+    if (!event.target.closest('.dropdown')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
